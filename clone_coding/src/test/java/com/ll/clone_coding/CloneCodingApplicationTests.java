250bde5fab033b9a86fec41dcb48f39ebebd8cc6
leeyumin626 <leeym7404@naver.com>
2025-09-28 17:01:30 +0900

    초기세팅

---
+++ clone_coding/src/test/java/com/ll/clone_coding/CloneCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.clone_coding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloneCodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
