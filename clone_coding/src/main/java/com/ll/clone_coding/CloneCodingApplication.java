722168bc9cc1e784497d0dfcad92ab7c700654d4
leewater <leewatertrue@gmail.com>
2025-09-30 17:25:28 +0900

    feat: 초기 설정 저장

--- clone_coding/src/main/java/com/ll/clone_coding/CloneCodingApplication.java
+++ clone_coding/src/main/java/com/ll/clone_coding/CloneCodingApplication.java
@@ -2,8 +2,10 @@ package com.ll.clone_coding;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class CloneCodingApplication {
 
 	public static void main(String[] args) {
